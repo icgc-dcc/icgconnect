24f7361c3674c781da30f7f41e82fbba67a06266
Brice Aminou <brice.aminou@gmail.com>
2018-01-11 16:51:59 -0500

    Song function added to validate json schema

--- icgconnect/icgc/song/__init__.py
+++ icgconnect/icgc/song/__init__.py
@@ -1,6 +1,9 @@
-import urllib, json
+import urllib, json, jsonschema
 
 def get_schema():
     url = "https://raw.githubusercontent.com/overture-stack/SONG/develop/song-server/src/main/resources/schemas/sequencingRead.json"
     response = urllib.urlopen(url)
     return  json.loads(response.read())
+
+def validate_schema(json_text):
+    jsonschema.validate(json_text, get_schema())
